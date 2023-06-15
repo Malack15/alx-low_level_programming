1793fb1e9d65f4dc8580042b7965d62bd7d7f431
Malack15 <malackokeiga@gmail.com>
2023-06-15 14:06:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
 /**
-* main - A program that prints a line standard error
-* Return: 1 (Success)
-*/
+ * main - Entry point
+ * Return: Always 1 (success)
+ */
 int main(void)
-{
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
