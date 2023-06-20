abd66b8518dacbb5bbc2608c35192df2ebb958e1
Malack15 <malackokeiga@gmail.com>
2023-06-20 11:52:03 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - Check Main
  * @a: An integer a
  * @b: An integer b
- * Description: Function that adds two integers 
+ * Description: Function that adds two integers
  * Return: Result to add a and b
  */
 int add(int a, int b)
