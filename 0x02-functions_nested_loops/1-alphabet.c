b60ce1d1251b2de1b1dc3e038369d74a5b939f1a
Malack15 <malackokeiga@gmail.com>
2023-06-20 09:41:11 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#incude "main.h"
+/**
+  *print_alphabet-function that prints alphabets
+  *
+  */
+void print_alphabet(void)
+{
+	int n = 97;
+	for (n = 97; n <= 122; n++)
+
+	{
+		_putchar(n);
+	}
+_putchar('\n');
+}
