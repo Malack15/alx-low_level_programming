70d3583f0d870a1ffb0c7329bd244c82bf6f88c5
Malack15 <malackokeiga@gmail.com>
2023-06-20 10:29:30 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
   * _islower - checks if c is lowercase
-  *
+  *@c: the character in ASCII code
   *Return: 1 if c is lowercase 0 if otherwise
   *
   */
 int _islower(int c)
 {
-	if (c >= 97 &&c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
