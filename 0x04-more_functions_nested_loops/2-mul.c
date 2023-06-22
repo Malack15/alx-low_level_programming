58eeb97f1a50a887b1baaa729a0ce566222fec7c
Malack15 <malackokeiga@gmail.com>
2023-06-22 04:52:59 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Check Main
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers
+ * Return: Result to add a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
