bc7e1e1f495809f4c2c7936ea0ac730657602d77
Malack15 <malackokeiga@gmail.com>
2023-07-13 13:03:49 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,45 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* string_nconcat - a function that concatenates two strings.
+* *string_nconcat - concatenates n bytes of a string to another string
+* @s1: string to append to
+* @s2: string to concatenate from
+* @n: number of bytes from s2 to concatenate to s1
 *
-* @s1: first char
-* @s2: secound char
-* @n: unsigned int
-*
-* Return: If the function fails, it should return NULL
+* Return: pointer to the resulting string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int a, b, z;
 char *s;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-if (s1 == NULL)
-{
-a = 0;
-}
+while (s1 && s1[len1])
+len1++;
+while (s2 && s2[len2])
+len2++;
+
+if (n < len2)
+s = malloc(sizeof(char) * (len1 + n + 1));
 else
-{
-for (a = 0; s1[a]; ++a)
-;
-}
-if (s2 == NULL)
-{
-b = 0;
-}
-else
-{
-for (b = 0; s2[b]; ++b)
-;
-}
-if (b > n)
-b = n;
-s = malloc(sizeof(char) * (a + b + 1));
-if (s == NULL)
+s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+if (!s)
 return (NULL);
-for (z = 0; z < a; z++)
-s[z] = s1[z];
-for (z = 0; z < b; z++)
-s[z + a] = s2[z];
-s[a + b] = '\0';
+
+while (i < len1)
+{
+s[i] = s1[i];
+i++;
+}
+
+while (n < len2 && i < (len1 + n))
+s[i++] = s2[j++];
+
+while (n >= len2 && i < (len1 + len2))
+s[i++] = s2[j++];
+
+s[i] = '\0';
+
 return (s);
 }
