caea3e9209705e7433aac4515fc53615b5d18ee8
Malack15 <malackokeiga@gmail.com>
2023-07-19 13:03:45 -0700

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,16 +13,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (size <= 0 || array == NULL || cmp == NULL)
+if (size > 0)
 {
-return (-1);
-}
-for (i = 0; i < size; i++)
+if (array != NULL && cmp != NULL)
 {
-if (cmp(*(array + i)))
+while (i < size)
 {
+if (cmp(array[i]))
 return (i);
+
+i++;
 }
 }
+}
+
 return (-1);
 }
