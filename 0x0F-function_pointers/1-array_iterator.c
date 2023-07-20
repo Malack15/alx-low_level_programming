7863a1c01dfba18bc14c07e9d64548223c405581
Malack15 <malackokeiga@gmail.com>
2023-07-20 11:29:52 -0700

    Beauty is variable, ugliness is constant

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
 * array_iterator - executes a function given as a parameter on each element
@@ -9,7 +8,8 @@
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
-{size_t i;
+{
+size_t i;
 
 if (array && action)
 {
