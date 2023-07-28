b86b094b776d5408fd4166aa4cc3974e156cddad
Malack15 <malackokeiga@gmail.com>
2023-07-28 11:47:08 -0700

    Linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* list_len - finds the number of elements in a linked list
+*list_len - a function that returns the number of elements in a linked list
 *@h: pointer to the list
 *
 *Return: the number of elements
